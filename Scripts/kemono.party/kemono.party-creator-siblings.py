8890aa014328e0e29a634b5790a7a3fe92f64ab5
QuintinMouser <154471663+QuintinMouser@users.noreply.github.com>
2023-12-20 13:45:21 -0800

    Use single quote strings for consistency

--- Scripts/kemono.party/kemono.party-creator-siblings.py
+++ Scripts/kemono.party/kemono.party-creator-siblings.py
@@ -15,13 +15,13 @@ def get_creators():
 
 creators = get_creators()
 if creators is None: exit(1)
-with open(OUT_FILE, 'a', encoding="utf-8") as file:
+with open(OUT_FILE, 'a', encoding='utf-8') as file:
     for creator in creators:
         name = creator['name']
         id = creator['id']
-        service = creator["service"]
+        service = creator['service']
         if name and id:
             if service == "fanbox": continue
             creator_name = name.replace('\n', ' ')
-            file.write(f"{service} id:{id}\ncreator:{creator_name}\n")
+            file.write(f'{service} id:{id}\ncreator:{creator_name}\n')
 
