f6163a9778319132658b563c0f144279c710464a
QuintinMouser <154471663+QuintinMouser@users.noreply.github.com>
2023-12-20 13:43:31 -0800

    Use constants vars, move get creators to function

--- Scripts/kemono.party/kemono.party-creator-siblings.py
+++ Scripts/kemono.party/kemono.party-creator-siblings.py
@@ -1,7 +1,21 @@
-import requests, json, os
+import requests, json, os, sys
 
-creators = json.loads(requests.get('https://kemono.party/api/v1/creators').text)
-with open("kemono.party_siblings.txt", 'a', encoding="utf-8") as file:
+API_URL = 'https://kemono.party/api/v1/creators'
+OUT_FILE = 'kemono.party_siblings.txt'
+
+def get_creators():
+    creators = None
+    try:
+        creators = json.loads(requests.get(API_URL).text)
+    except Exception as exc:
+        with sys.stderr as dest:
+            print(exc, file=dest)
+        print(f'Error fetching creators. Ensure URL "{API_URL}" is correct.')
+    return creators
+
+creators = get_creators()
+if creators is None: exit(1)
+with open(OUT_FILE, 'a', encoding="utf-8") as file:
     for creator in creators:
         name = creator['name']
         id = creator['id']
