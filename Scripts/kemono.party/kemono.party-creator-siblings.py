923ad03eac593be6c24c7327a4575a9722552e18
QuintinMouser <154471663+QuintinMouser@users.noreply.github.com>
2023-12-20 13:24:38 -0800

    Use file context manager

--- Scripts/kemono.party/kemono.party-creator-siblings.py
+++ Scripts/kemono.party/kemono.party-creator-siblings.py
@@ -1,14 +1,12 @@
 import requests, json, os
 
 creators = json.loads(requests.get('https://kemono.party/api/v1/creators').text)
-file = open("kemono.party_siblings.txt", 'a',encoding="utf-8")
+with open("kemono.party_siblings.txt", 'a', encoding="utf-8") as file:
+    for creator in creators:
+        name = creator['name']
+        id = creator['id']
+        service = creator["service"]
+        if name and id != "":
+            if service == "fanbox": continue
+            file.write(service + " id:" + id + "\ncreator:" + name.replace('\n', ' ') + "\n")
 
-for creator in creators:
-    name = creator['name']
-    id = creator['id']
-    service = creator["service"]
-    if name and id != "":
-        if service == "fanbox": continue
-        file.write(service + " id:" + id + "\ncreator:" + name.replace('\n', ' ') + "\n")
-
-file.close()
